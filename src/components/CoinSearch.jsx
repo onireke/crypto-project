0c8ee105f5110b86b69b6675f6d162e676e71751
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2023-05-21 15:14:18 +0100

    feat: navbar

---
+++ src/components/CoinSearch.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CoinSearch() {
+  return <div>CoinSearch</div>;
+}
+
+export default CoinSearch;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ThemeToggle from "./ThemeToggle";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import "../styles/componentStyles/Navbar.scss";
+import { useState } from "react";
 
 function Navbar() {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
     <div className="rounded-div">
       <Link to="/">
@@ -22,13 +28,19 @@ function Navbar() {
         </Link>
       </div>
       {/*menu icon */}
-      <div className="menu">
-        <AiOutlineMenu />
+      <div className="menu" onClick={handleNav}>
+        {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={20} />}
       </div>
 
       {/* mobile menu */}
 
-      <div className="mobile-menu">
+      <div
+        className={
+          nav
+            ? "md:hidden fixed flex flex-col justify-between h-[90%] left-0  top-20 items-center bg-primary w-full ease-in duration-300 z-10"
+            : "fixed left-[-100%] top-20 flex flex-col items-center justify-between ease-in duration-300"
+        }
+      >
         <ul className="list-container">
           <li className="list">
             <Link to="/">Home</Link>{" "}
@@ -36,17 +48,17 @@ function Navbar() {
           <li className="list">
             <Link to="/">Account</Link>{" "}
           </li>
-          <li>
+          <li className="py-2">
             <ThemeToggle />
           </li>
         </ul>
 
-        <div>
+        <div className="sign-div">
           <Link to="/signin">
-            <button>Sign In</button>
+            <button className="mobile-signin">Sign In</button>
           </Link>
           <Link to="/signup">
-            <button>Sign Up</button>
+            <button className="mobile-signup">Sign Up</button>
           </Link>
         </div>
       </div>
