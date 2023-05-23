a0efdbe9e1f56e3b4ba25f8003c2efd95cb65d65
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2023-05-23 10:47:54 +0100

    feat: coinsearch

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,7 @@ import { useEffect } from "react";
 function App() {
   const [coins, setCoins] = useState([]);
 
-  const url =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en";
+  const url = "https://api.coinpaprika.com/v1/tickers";
 
   useEffect(() => {
     axios.get(url).then((response) => {
